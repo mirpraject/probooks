a96825b977858df225c33a1f50f1646fa70b6f87
Ibragimov <ibragimov@example.com>
2026-08-30 17:43:32 +0300

    fix: disable django-axes during testing

--- config/settings.py
+++ config/settings.py
@@ -182,7 +182,9 @@ AUTHENTICATION_BACKENDS = [
 
 # --- Axes ---
 TESTING = 'test' in sys.argv
-AXES_ENABLED = config('AXES_ENABLED', default=True, cast=bool)
+# Disable Axes during tests since it breaks DRF API test logins
+IS_TESTING = 'test' in sys.argv
+AXES_ENABLED = False if IS_TESTING else config('AXES_ENABLED', default=True, cast=bool)
 AXES_FAILURE_LIMIT = 5
 AXES_COOLOFF_TIME = timedelta(minutes=1)
 AXES_RESET_ON_SUCCESS = True
