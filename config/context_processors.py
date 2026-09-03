4f71e64abb451b14c8dc455ae8ea459d2dc50d35
Ibragimov <ibragimov@example.com>
2026-09-03 11:23:09 +0300

    Implement 4-season dynamic theme

--- config/context_processors.py
+++ config/context_processors.py
@@ -1,8 +1,24 @@
 from django.conf import settings
 
 
+from datetime import date
+
 def theme(request):
-    return {'theme': request.COOKIES.get('theme', 'light')}
+    theme_cookie = request.COOKIES.get('theme')
+    if theme_cookie:
+        return {'theme': theme_cookie}
+    
+    month = date.today().month
+    if month in (3, 4, 5):
+        default_theme = 'spring'
+    elif month in (6, 7, 8):
+        default_theme = 'summer'
+    elif month in (9, 10, 11):
+        default_theme = 'autumn'
+    else:
+        default_theme = 'winter'
+        
+    return {'theme': default_theme}
 
 
 def current_language(request):
